13802bc1998211ae111d05bddc1ad1070ed2252d
Doct03 <84016607+Doct03@users.noreply.github.com>
2021-05-29 13:48:59 +0300

    Add files via upload

---
+++ js7/js7_3.js
@@ -0,0 +1,9 @@
+function btnClick()
+        {
+            if(document.Test1.Sex[0].checked){
+                document.Test1.Sex[1].click();
+            }else{
+                document.Test1.Sex[0].click();
+            }
+        }
+        //-->
